690c5355f361344d9a4ab9c6aaaaa867e52e1fa9
rockomatthews <rob@robmatthe.ws>
2025-04-19 17:00:31 -0600

    fixed vercel shit

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -266,7 +266,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
